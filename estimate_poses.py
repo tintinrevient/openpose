cb92076b6cbb1a02a8e5b2d6c9ecf8ccb3aaafec
Zhao Shu <zhaobenben007@googlemail.com>
2021-03-28 22:23:21 +0200

    update

---
+++ estimate_poses.py
@@ -0,0 +1,26 @@
+import os
+import argparse
+import subprocess
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='OpenPose')
+    parser.add_argument('--input', help='Path to the input directory')
+    args = parser.parse_args()
+
+    for root, dirs, files in os.walk(args.input):
+        for name in dirs:
+
+            indir = os.path.join(root, name)
+            fname = indir[indir.find('/')+1:]
+            outdir = os.path.join('output', fname)
+
+            print('indir:', indir)
+            print('outdir:', outdir)
+
+            if not os.path.exists(outdir):
+                os.makedirs(outdir)
+
+            bash_command = subprocess.run(['./build/examples/openpose/openpose.bin',
+                                           '--image_dir', indir,
+                                           '--write_images', outdir], check=True, text=True)
